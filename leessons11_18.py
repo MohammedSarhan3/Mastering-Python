a4bbe6d5a13be4711ada02c5922f3f4e2f32f1ad
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-24 06:49:14 +0200

    Add First Task

---
+++ leessons11_18.py
@@ -0,0 +1,8 @@
+#1
+#name= "'osama'"
+age = '"18"'
+coun= "egypt"
+
+#print("Hello" +name + ", How You Doing \\" 
+#'""" ' 'Your Age Is'+ age +'"'" +
+#\And Your Country Is:" + coun)
---
+++ leessons1_10.py
@@ -0,0 +1,10 @@
+a= "mohammad"
+b= "18"
+c= "palstine"
+print("name is : "+ a )
+print("Country: " + c)
+print("age: " + b)
+print("Hello, My Name Is "+ a+" And Iam " +b +" Years Old and I Live in " + c)
+print(type(a))
+print(type(b))
+print(type(c))
