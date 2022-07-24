7f6e9565f247119b4c53c90e387141d760cce608
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-24 06:50:00 +0200

    Add Secound Task

--- leessons11_18.py
+++ leessons11_18.py
@@ -1,3 +1,4 @@
+Add First Task"
 #1
 #name= "'osama'"
 age = '"18"'
@@ -6,3 +7,14 @@ coun= "egypt"
 #print("Hello" +name + ", How You Doing \\" 
 #'""" ' 'Your Age Is'+ age +'"'" +
 #\And Your Country Is:" + coun)
+
+#2
+name = 'Elzero'
+# Needed Output
+# Second Letter Is "l"
+print(name[1])
+# Third Letter Is "z"
+print(name[2])
+# Last Letter Is "o"
+print(name[5])
+
