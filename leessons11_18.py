326d975d6d4c1312cfbe04f5e08aab8aaef0beac
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-24 06:56:50 +0200

    Add 13th Task

--- leessons11_18.py
+++ leessons11_18.py
@@ -1,4 +1,4 @@
-Add First Task
+
 #1
 #name= "'osama'"
 age = '"18"'
@@ -100,5 +100,15 @@ msg = "I <3 Python And Although <3 Elzero Web School"
 # Needed Output
 print(msg.replace("<3", "Love"))
 # I Love Python And Although Love Elzero Web School
+
+#13
+
+name = "Osama"
+age = 38
+country = "Egypt"
+
+# Needed Output Using f""
+# My Name Is Osama, And My Age Is 38, And My Country Is Egypt
+print(f"My Name Is {name}, And My Age Is {age}, And My Country Is {country}")
 
 
