7c50a89f16a4fd2874c0e8200e4cbca02200d594
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-08-03 17:58:02 +0200

    Task [2]

--- lessons33_40.py
+++ lessons33_40.py
@@ -11,3 +11,39 @@ print(bool(True))
 print(bool([1, 2, 3, 4, 5]))
 
 print("=" * 50)
+
+
+#Task [1]
+# True Values
+
+print(bool("Osama"))
+print(bool(100))
+print(bool(100.95))
+print(bool(True))
+print(bool([1, 2, 3, 4, 5]))
+
+print("=" * 50)
+
+# False Values
+
+print(bool(0))
+print(bool(""))
+print(bool(''))
+print(bool([]))
+print(bool(False))
+print(bool(()))
+print(bool({}))
+print(bool(None))
+
+print("=" * 50)
+# Task [2]
+html = 80
+css = 60
+javascript = 70
+
+# Needed Output
+#True
+print(html>50 and css>50 and javascript>50)
+
+
+print("=" * 50)
