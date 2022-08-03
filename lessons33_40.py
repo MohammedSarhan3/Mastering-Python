1b15c8c37469ae6bc973be003e0cf28c3efb1f76
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-08-03 17:57:13 +0200

    Assignments For Lessons 33 To 37 Task [1]

---
+++ lessons33_40.py
@@ -0,0 +1,13 @@
+#Assignments For Lessons 33 To 37
+
+print("Assignments For Lessons 33 To 37")
+#Task [1]
+# True Values
+
+print(bool("Osama"))
+print(bool(100))
+print(bool(100.95))
+print(bool(True))
+print(bool([1, 2, 3, 4, 5]))
+
+print("=" * 50)
