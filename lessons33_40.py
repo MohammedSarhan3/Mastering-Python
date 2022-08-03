c1012c6ad89abcf4a01973de046fbc410c4c4443
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-08-03 17:58:33 +0200

    Task [3]

--- lessons33_40.py
+++ lessons33_40.py
@@ -47,3 +47,19 @@ print(html>50 and css>50 and javascript>50)
 
 
 print("=" * 50)
+
+
+# Task [3]
+num_one = 10
+num_two = 20
+num = 20
+
+# Needed Output
+
+#True
+print(num>num_one or num > num_two)
+#False
+print(num>num_one and num > num_two)
+
+
+print("=" * 50)
