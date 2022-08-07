58a07e0a5e8cb9cc0890ff4db0907c8be358f7f9
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-08-07 12:49:57 +0200

    Task [2]

--- lessons41_46.py
+++ lessons41_46.py
@@ -1,2 +1,31 @@
 #Assignments For Lessons 41 To 46
 # Task [1]
+
+# Inputs
+'''
+#num1 = int(input("Enter The Fisrt Number: ").strip())
+#num2 = int(input("Enter The Secound Number: ").strip())
+#operation = input("Enter Operation: ").strip()
+#"+" Or "-" Or "*" Or "/" Or "%"
+# Needed Output
+if operation == "+":
+    nums =num1 + num2
+    print(f"[{num1}][{operation}][{num2}][={nums}]")
+if operation == "-":
+    nums =num1 - num2
+    print(f"[{num1}][{operation}][{num2}][={nums}]")
+if operation == "*":
+    nums =num1 * num2
+    print(f"[{num1}][{operation}][{num2}][={nums}]")
+if operation == "/":
+    nums =num1 / num2
+    print(f"[{num1}][{operation}][{num2}][={nums}]")
+if operation == "%":
+    nums =num1 % num2
+    print(f"[{num1}][{operation}][{num2}][={nums}]")
+
+# [num1 20] [operation +] [num2]
+# Example One 20 + 40 = 60
+# Example Two 20 * 40 = 800
+
+'''
