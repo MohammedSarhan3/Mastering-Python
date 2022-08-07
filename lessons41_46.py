ef71b8f52f2578120f2f6335a1fb9d8f8681d99a
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-08-07 12:51:51 +0200

    Task [4]

--- lessons41_46.py
+++ lessons41_46.py
@@ -57,3 +57,19 @@ else:
 #"Your Age In Weeks Is 1824 Weeks" # Weeks Example
 
 '''
+
+#Task [4]
+# Input Example One "Egypt"
+# Input Example Two "Ghana"
+
+country = input("Input Your Country:  ").strip().capitalize()
+countries = ["Egypt", "Palestine", "Syria", "Yemen", "KSA", "USA", "Bahrain", "England"]
+price = 100
+discount = price - 30
+if country in countries:
+    print(f"Your Country Eligible For Discount And The Price After Discount Is ${discount}")
+else:
+    print("Your Country Not Eligible For Discount And The Price Is $100")
+# Needed Output
+"Your Country Eligible For Discount And The Price After Discount Is $70" # Egypt Example
+"Your Country Not Eligible For Discount And The Price Is $100" # Ghana Example
